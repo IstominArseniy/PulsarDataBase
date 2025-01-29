18b434c91c11ed880fb6abec6ce07905f620cb16
Istomin Arseniy <istomin.aiu@phystech.edu>
2025-01-29 15:28:48 +0300

    Linear polarisation is now correct for MeerKAT. PA is still incorrect!!!

--- DataLoaders.py
+++ DataLoaders.py
@@ -35,10 +35,12 @@ def load_FAST_data(file_name):
 def load_MeerKAT_mean_data(file_name):
     hdul = fits.open(file_name)
     DM = hdul[4].header['DM']
+    RM = hdul[4].header['RM']
     DC = DM / 2.41e-16
     Jname = hdul[0].header['SRC_NAME']
     P = hdul[4].header['PERIOD']
     freqs = hdul[4].data[0]['DAT_FREQ'] * 1e6
+    print(RM, freqs)
     N = hdul[4].data[0]['DATA'][0][0].shape[0]
     profile_data = np.zeros((6, N))
     add_shift = (np.argmax(hdul[4].data[0]['DATA'][0][0]) - N//4) 
@@ -47,13 +49,19 @@ def load_MeerKAT_mean_data(file_name):
             hdul[4].data[0]['DATA'][stocks][channel] = np.roll(hdul[4].data[0]['DATA'][stocks][channel], int(DC * (1/freqs[0]**2 - 1/freqs[channel]**2) * N / P) - add_shift)
     # TEST global profile shift
     for channel in range(8):
-        profile_data[0] += (hdul[4].data[0]['DATA'][0][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][0][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*0+channel] * (hdul[4].data[0]['DAT_WTS'][channel] / np.sum(hdul[4].data[0]['DAT_WTS']))
-        profile_data[1] += (hdul[4].data[0]['DATA'][1][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][1][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*1+channel] * (hdul[4].data[0]['DAT_WTS'][channel] / np.sum(hdul[4].data[0]['DAT_WTS']))
-        profile_data[2] += (hdul[4].data[0]['DATA'][2][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][2][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*2+channel] * (hdul[4].data[0]['DAT_WTS'][channel] / np.sum(hdul[4].data[0]['DAT_WTS']))
-        profile_data[3] += (hdul[4].data[0]['DATA'][3][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][3][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*3+channel] * (hdul[4].data[0]['DAT_WTS'][channel] / np.sum(hdul[4].data[0]['DAT_WTS']))
-    profile_data[4] = np.sqrt(profile_data[1]**2 + profile_data[2]**2)
+        weight = hdul[4].data[0]['DAT_WTS'][channel] / np.sum(hdul[4].data[0]['DAT_WTS'])
+        profile_data[0] += (hdul[4].data[0]['DATA'][0][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][0][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*0+channel] * weight
+        Q = (hdul[4].data[0]['DATA'][1][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][1][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*1+channel] 
+        profile_data[1] += Q * weight
+        U = (hdul[4].data[0]['DATA'][2][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][2][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*2+channel] 
+        profile_data[2] += U * weight
+        profile_data[3] += (hdul[4].data[0]['DATA'][3][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][3][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*3+channel] * weight
+        profile_data[4] += weight * np.sqrt(Q**2 + U**2)
+        beta = RM * ((299792458 / freqs[0])**2 - (299792458 / freqs[channel])**2)
+        print(beta)
+        profile_data[5] += (0.5 * np.arctan2(U, Q)) * 180 / np.pi * weight
     profile_data[4] -= aux.noise_mean(profile_data[4])
-    profile_data[5] = 0.5 * np.arctan2(profile_data[2], profile_data[1]) * 180 / np.pi
+    # profile_data[5] = 0.5 * np.arctan2(profile_data[2], profile_data[1]) * 180 / np.pi
     return profile_data
     
 
