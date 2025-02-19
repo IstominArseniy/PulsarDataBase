57ba1ccd4f7f2913c08174599c7f706e34ccb8ae
Istomin Arseniy <istomin.aiu@phystech.edu>
2025-02-19 12:56:58 +0300

    update L and PA calculation for MeerKAT mean profiles

--- DataLoaders.py
+++ DataLoaders.py
@@ -47,32 +47,26 @@ def load_MeerKAT_mean_data(file_name):
         for stocks in range(4):
             hdul[4].data[0]['DATA'][stocks][channel] = np.roll(hdul[4].data[0]['DATA'][stocks][channel], int(DC * (1/freqs[0]**2 - 1/freqs[channel]**2) * N / P) - add_shift)
     # TEST global profile shift
-    sinPA = 0
-    cosPA = 0
     for channel in range(8):
         weight = hdul[4].data[0]['DAT_WTS'][channel] / np.sum(hdul[4].data[0]['DAT_WTS'])
         #---------I, Q, U, V--------------
         profile_data[0] += (hdul[4].data[0]['DATA'][0][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][0][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*0+channel] * weight
         Q = (hdul[4].data[0]['DATA'][1][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][1][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*1+channel] 
-        profile_data[1] += Q * weight
-        U = (hdul[4].data[0]['DATA'][2][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][2][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*2+channel] 
-        profile_data[2] += U * weight
+        U = (hdul[4].data[0]['DATA'][2][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][2][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*2+channel]
+        beta = RM * ((299792458 / freqs[0])**2 - (299792458 / freqs[channel])**2) 
+        profile_data[1] += weight * (np.cos(2 * beta) * Q - np.sin(2 * beta) * U)
+        profile_data[2] += weight * (np.sin(2 * beta) * Q + np.cos(2 * beta) * U)
         profile_data[3] += (hdul[4].data[0]['DATA'][3][channel].astype('int64') - aux.noise_mean(hdul[4].data[0]['DATA'][3][channel].astype('int64'))) * hdul[4].data[0]['DAT_SCL'][8*3+channel] * weight
-        #----------L, PA------------------
-        profile_data[4] += weight * np.sqrt(Q**2 + U**2)
-        beta = RM * ((299792458 / freqs[0])**2 - (299792458 / freqs[channel])**2)
-        # profile_data[5] += aux.shift_angle(beta + 0.5 * np.arctan2(U, Q)) * 180 / np.pi * weight
-        sinPA += np.sin(2 * beta + np.arctan2(U, Q)) * weight
-        cosPA += np.cos(2 * beta + np.arctan2(U, Q)) * weight
-        # profile_data[5] += (beta + 0.5 * np.arctan2(U, Q)) * 180 / np.pi * weight
-    profile_data[4] -= aux.noise_mean(profile_data[4])
-    profile_data[5] = aux.shift_angle(0.5 * np.arctan2(sinPA, cosPA)) * 180 / np.pi
+    #----------L, PA------------------
+    profile_data[4] = np.sqrt(profile_data[1]**2 + profile_data[2]**2) - aux.noise_mean(np.sqrt(profile_data[1]**2 + profile_data[2]**2))
+    #TODO better noise bias elimination
+    profile_data[5] = aux.shift_angle(0.5 * np.arctan2(profile_data[2], profile_data[1])) * 180 / np.pi
+    # profile_data[5] = aux.shift_angle(0.5 * np.arctan2(sinPA, cosPA)) * 180 / np.pi
     return profile_data
     
 
 def load_MeerKAT_channel_data(file_name, channel):
     hdul = fits.open(file_name)
-    print(hdul[4].header)
     DM = hdul[4].header['DM']
     DC = DM / 2.41e-16
     Jname = hdul[0].header['SRC_NAME']
